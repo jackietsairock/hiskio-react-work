3ace03fdfc22265fd68f4c8079f79e6f52c7c1e6
jackietsai <jackietsairock@gmail.com>
2021-08-19 09:19:08 +0800

    Updates

---
+++ precache-manifest.723d47a10080aa072101a1379ab6cce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "739d3bded172d0843600",
+    "url": "/hiskio-react-work/static/css/main.2c472182.chunk.css"
+  },
+  {
+    "revision": "739d3bded172d0843600",
+    "url": "/hiskio-react-work/static/js/main.bd99ec56.chunk.js"
+  },
+  {
+    "revision": "863a585a41dd5f9be4a4",
+    "url": "/hiskio-react-work/static/js/runtime~main.4f7bc0b9.js"
+  },
+  {
+    "revision": "0bb90fa119c16f0060d6",
+    "url": "/hiskio-react-work/static/css/2.9423e50e.chunk.css"
+  },
+  {
+    "revision": "0bb90fa119c16f0060d6",
+    "url": "/hiskio-react-work/static/js/2.6b6f314d.chunk.js"
+  },
+  {
+    "revision": "05e4648333f9bac9465ee6c2206a68a4",
+    "url": "/hiskio-react-work/index.html"
+  }
+];
